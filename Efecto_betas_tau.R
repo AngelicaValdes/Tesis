def700af5941643cc787f14dd930cba5a18a30d3
AngelicaValdes <91926017+AngelicaValdes@users.noreply.github.com>
2021-10-08 14:07:24 -0300

    Update Efecto_betas_tau.R

--- Efecto_betas_tau.R
+++ Efecto_betas_tau.R
@@ -12,8 +12,6 @@
       cat("\nCXXFLAGS += -Wno-ignored-attributes -Wno-deprecated-declarations", 
           file = M, sep = "\n", append = TRUE)
     }
-    setwd("H:/Dropbox/yan shang/delays angelica")
-    #setwd("C:/Users/Angie/Dropbox/delays angelica")
     library(readxl)
     library(dplyr)
     library("rstan")
@@ -21,28 +19,28 @@
     options(mc.cores = parallel::detectCores())
     
     #guardar los datos de beta, beta_0
-    load("H:/Dropbox/yan shang/delays angelica/modelo_6_5.RData") #jerarquico
-    load("H:/Dropbox/yan shang/delays angelica/modelo_12_2.RData") #independiente
-    load("H:/Dropbox/yan shang/delays angelica/modelo_18.RData") #naive
+    load("/modelo_6_5.RData") #jerarquico
+    load("/modelo_12_2.RData") #independiente
+    load("/modelo_18.RData") #naive
     beta_0 <- summary(reg_fit2, "beta_0", probs = NULL)$summary
     beta1 <- summary(reg_fit2, "beta1", probs = NULL)$summary
     beta2 <- summary(reg_fit2, "beta2", probs = NULL)$summary
     
     #se cargan los datos necesarios
     #Para datos de aprendizaje jerarquico
-    load('H:/Dropbox/yan shang/delays angelica/mydata_server_14.RData')
+    load('/mydata_server_14.RData')
     BD2 <- read_excel("Base_modelo_15.xlsx" , guess_max = 50000)
-    load('H:/Dropbox/yan shang/delays angelica/mydata_server_3.RData')
+    load('/mydata_server_3.RData')
     BD3<-BD
     #Para datos de aprendizaje independiente
-    #load('H:/Dropbox/yan shang/delays angelica/mydata_server_13.RData')
+    #load('/mydata_server_13.RData')
     #BD2 <- read_excel("Base_modelo_14.xlsx" , guess_max = 50000)
-    load('H:/Dropbox/yan shang/delays angelica/mydata_server_5.RData')
+    load('/mydata_server_5.RData')
     BD3<-BD
     #Para datos de método Naïve
-    #load('H:/Dropbox/yan shang/delays angelica/mydata_server_10.RData')
+    #load('/mydata_server_10.RData')
     #BD2 <- read_excel("Base_modelo_13.xlsx" , guess_max = 50000)
-    load('H:/Dropbox/yan shang/delays angelica/mydata_server_9.RData')
+    load('/mydata_server_9.RData')
     BD3<-BD
     
     BD2$tau_1<-BD2$tau_1/24
@@ -260,4 +258,4 @@
     efecto$xi<-xi
     
     library(writexl)
-    write_xlsx(efecto, "H:/Dropbox/yan shang/delays angelica/jerarquico_tau_desv_theta.xlsx")
+    write_xlsx(efecto, "/jerarquico_tau_desv_theta.xlsx")
